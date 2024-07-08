ed8a81a4d28d6cc1b35267d4239bb5269547963c
sara-samy <intermsofart@hotmail.com>
2024-07-08 21:09:18 +0200

    Solution for exercise 003_a_triangle_mesh

--- 003_a_triangle_mesh/exercise/simple_cube.py
+++ 003_a_triangle_mesh/exercise/simple_cube.py
@@ -1,4 +1,6 @@
 import numpy as np
+import polyscope as ps
+
 
 def simple_cube():
     """Construct a triangle mesh for a single cube.
@@ -7,15 +9,34 @@ def simple_cube():
     F describing a triangle mesh of a single cube.
     """
 
-    V = np.array([[?, ?, ?],
-        [?, ?, ?],
-        [?, ?, ?],
-        [?, ?, ?],
-        [?, ?, ?],
-        [?, ?, ?],
-        [?, ?, ?],
-        [?, ?, ?]])
+    V = np.array(
+        [
+            [0.0, 0.0, 0.0],
+            [1.0, 0.0, 0.0],
+            [0.0, 1.0, 0.0],
+            [1.0, 1.0, 0.0],
+            [0.0, 0.0, 1.0],
+            [1.0, 0.0, 1.0],
+            [0.0, 1.0, 1.0],
+            [1.0, 1.0, 1.0],
+        ]
+    )
 
-    F = np.array([[]])
+    F = np.array(
+        [
+            [0, 2, 3, 1],
+            [0, 2, 6, 4],
+            [0, 4, 5, 1],
+            [1, 5, 7, 3],
+            [4, 5, 7, 6],
+            [2, 3, 7, 6],
+        ]
+    )
 
     return V, F
+
+
+V, F = simple_cube()
+ps.init()
+ps.register_surface_mesh("Cube", V, F)
+ps.show()
